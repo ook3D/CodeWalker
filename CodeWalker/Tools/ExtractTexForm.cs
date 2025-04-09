c00065c04b2a1bae0eaf91694d8db31762df74e0
ook3d <47336113+ook3D@users.noreply.github.com>
2025-04-09 17:46:03 -0400

    make texture extract tool asynchronous

--- CodeWalker/Tools/ExtractTexForm.cs
+++ CodeWalker/Tools/ExtractTexForm.cs
@@ -80,7 +80,7 @@ namespace CodeWalker.Tools
             catch { }
         }
 
-        private void ExtractButton_Click(object sender, EventArgs e)
+        private async void ExtractButton_Click(object sender, EventArgs e)
         {
             if (InProgress) return;
 
@@ -99,42 +99,38 @@ namespace CodeWalker.Tools
                 MessageBox.Show("Folder doesn't exist: " + OutputFolderTextBox.Text);
                 return;
             }
-            //if (Directory.GetFiles(OutputFolderTextBox.Text, "*.ysc", SearchOption.AllDirectories).Length > 0)
-            //{
-            //    if (MessageBox.Show("Output folder already contains .ysc files. Are you sure you want to continue?", "Output folder already contains .ysc files", MessageBoxButtons.OKCancel) != DialogResult.OK)
-            //    {
-            //        return;
-            //    }
-            //}
 
             InProgress = true;
             AbortOperation = false;
 
             string searchpath = FolderTextBox.Text;
             string outputpath = OutputFolderTextBox.Text;
-            string replpath = searchpath + "\\";
             bool bytd = YtdChecBox.Checked;
             bool bydr = YdrCheckBox.Checked;
             bool bydd = YddCheckBox.Checked;
             bool byft = YftCheckBox.Checked;
             bool gen9 = GTAFolder.IsGen9Folder(searchpath);
 
-            Task.Run(() =>
+            await Task.Run(async () =>
             {
-
                 UpdateExtractStatus("Keys loaded.");
-
-
 
                 RpfManager rpfman = new RpfManager();
                 rpfman.Init(searchpath, gen9, UpdateExtractStatus, UpdateExtractStatus);
 
-
                 UpdateExtractStatus("Beginning texture extraction...");
                 StringBuilder errsb = new StringBuilder();
-                foreach (RpfFile rpf in rpfman.AllRpfs)
+
+                foreach (var rpf in rpfman.AllRpfs)
                 {
-                    foreach (RpfEntry entry in rpf.AllEntries)
+                    if (AbortOperation)
+                    {
+                        UpdateExtractStatus("Operation aborted");
+                        InProgress = false;
+                        return;
+                    }
+
+                    foreach (var entry in rpf.AllEntries)
                     {
                         if (AbortOperation)
                         {
@@ -142,120 +138,129 @@ namespace CodeWalker.Tools
                             InProgress = false;
                             return;
                         }
+
                         try
                         {
                             if (bytd && entry.NameLower.EndsWith(".ytd"))
                             {
-                                UpdateExtractStatus(entry.Path);
-                                YtdFile ytd = rpfman.GetFile<YtdFile>(entry);
-                                if (ytd == null) throw new Exception("Couldn't load file.");
-                                if (ytd.TextureDict == null) throw new Exception("Couldn't load texture dictionary.");
-                                if (ytd.TextureDict.Textures == null) throw new Exception("Couldn't load texture dictionary texture array.");
-                                if (ytd.TextureDict.Textures.data_items == null) throw new Exception("Texture dictionary had no entries...");
-                                foreach (var tex in ytd.TextureDict.Textures.data_items)
-                                {
-                                    SaveTexture(tex, entry, outputpath);
-                                }
+                                await ProcessYtdFileAsync(entry, rpfman, outputpath);
                             }
                             else if (bydr && entry.NameLower.EndsWith(".ydr"))
                             {
-                                UpdateExtractStatus(entry.Path);
-                                YdrFile ydr = rpfman.GetFile<YdrFile>(entry);
-                                if (ydr == null) throw new Exception("Couldn't load file.");
-                                if (ydr.Drawable == null) throw new Exception("Couldn't load drawable.");
-                                if (ydr.Drawable.ShaderGroup != null)
-                                {
-                                    var ydrtd = ydr.Drawable.ShaderGroup.TextureDictionary;
-                                    if ((ydrtd != null) && (ydrtd.Textures != null) && (ydrtd.Textures.data_items != null))
-                                    {
-                                        foreach (var tex in ydrtd.Textures.data_items)
-                                        {
-                                            SaveTexture(tex, entry, outputpath);
-                                        }
-                                    }
-                                }
+                                await ProcessYdrFileAsync(entry, rpfman, outputpath);
                             }
                             else if (bydd && entry.NameLower.EndsWith(".ydd"))
                             {
-                                UpdateExtractStatus(entry.Path);
-                                YddFile ydd = rpfman.GetFile<YddFile>(entry);
-                                if (ydd == null) throw new Exception("Couldn't load file.");
-                                //if (ydd.DrawableDict == null) throw new Exception("Couldn't load drawable dictionary.");
-                                //if (ydd.DrawableDict.Drawables == null) throw new Exception("Drawable dictionary had no items...");
-                                //if (ydd.DrawableDict.Drawables.data_items == null) throw new Exception("Drawable dictionary had no items...");
-                                if ((ydd.Dict==null)||(ydd.Dict.Count==0)) throw new Exception("Drawable dictionary had no items...");
-                                foreach (var drawable in ydd.Dict.Values)
-                                {
-                                    if (drawable.ShaderGroup != null)
-                                    {
-                                        var ydrtd = drawable.ShaderGroup.TextureDictionary;
-                                        if ((ydrtd != null) && (ydrtd.Textures != null) && (ydrtd.Textures.data_items != null))
-                                        {
-                                            foreach (var tex in ydrtd.Textures.data_items)
-                                            {
-                                                SaveTexture(tex, entry, outputpath);
-                                            }
-                                        }
-                                    }
-                                }
+                                await ProcessYddFileAsync(entry, rpfman, outputpath);
                             }
                             else if (byft && entry.NameLower.EndsWith(".yft"))
                             {
-                                UpdateExtractStatus(entry.Path);
-                                YftFile yft = rpfman.GetFile<YftFile>(entry);
-                                if (yft == null) throw new Exception("Couldn't load file.");
-                                if (yft.Fragment == null) throw new Exception("Couldn't load fragment.");
-                                if (yft.Fragment.Drawable != null)
-                                {
-                                    if (yft.Fragment.Drawable.ShaderGroup != null)
-                                    {
-                                        var ydrtd = yft.Fragment.Drawable.ShaderGroup.TextureDictionary;
-                                        if ((ydrtd != null) && (ydrtd.Textures != null) && (ydrtd.Textures.data_items != null))
-                                        {
-                                            foreach (var tex in ydrtd.Textures.data_items)
-                                            {
-                                                SaveTexture(tex, entry, outputpath);
-                                            }
-                                        }
-                                    }
-                                }
+                                await ProcessYftFileAsync(entry, rpfman, outputpath);
                             }
                         }
                         catch (Exception ex)
                         {
                             string err = entry.Name + ": " + ex.Message;
                             UpdateExtractStatus(err);
-                            errsb.AppendLine(err);
+                            lock (errsb)
+                            {
+                                errsb.AppendLine(err);
+                            }
                         }
-
                     }
                 }
-
-                File.WriteAllText(outputpath + "\\_errors.txt", errsb.ToString());
 
                 UpdateExtractStatus("Complete.");
                 InProgress = false;
             });
         }
 
-        private void SaveTexture(Texture tex, RpfEntry entry, string folder)
+        private async Task ProcessYtdFileAsync(RpfEntry entry, RpfManager rpfman, string outputpath)
         {
+            UpdateExtractStatus(entry.Path);
+            YtdFile ytd = rpfman.GetFile<YtdFile>(entry);
+            if (ytd == null || ytd.TextureDict == null || ytd.TextureDict.Textures == null || ytd.TextureDict.Textures.data_items == null)
+                throw new Exception("Couldn't load texture dictionary.");
 
-            //DirectXTex
+            foreach (var tex in ytd.TextureDict.Textures.data_items)
+            {
+                await SaveTextureAsync(tex, entry, outputpath);
+            }
+        }
 
+        private async Task ProcessYdrFileAsync(RpfEntry entry, RpfManager rpfman, string outputpath)
+        {
+            UpdateExtractStatus(entry.Path);
+            YdrFile ydr = rpfman.GetFile<YdrFile>(entry);
+            if (ydr == null || ydr.Drawable == null || ydr.Drawable.ShaderGroup == null)
+                throw new Exception("Couldn't load drawable or shader group.");
+
+            var ydrtd = ydr.Drawable.ShaderGroup.TextureDictionary;
+            if (ydrtd != null && ydrtd.Textures != null && ydrtd.Textures.data_items != null)
+            {
+                foreach (var tex in ydrtd.Textures.data_items)
+                {
+                    await SaveTextureAsync(tex, entry, outputpath);
+                }
+            }
+        }
+
+        private async Task ProcessYddFileAsync(RpfEntry entry, RpfManager rpfman, string outputpath)
+        {
+            UpdateExtractStatus(entry.Path);
+            YddFile ydd = rpfman.GetFile<YddFile>(entry);
+            if (ydd == null || ydd.Dict == null || ydd.Dict.Count == 0)
+                throw new Exception("Drawable dictionary had no items.");
+
+            foreach (var drawable in ydd.Dict.Values)
+            {
+                if (drawable.ShaderGroup != null)
+                {
+                    var ydrtd = drawable.ShaderGroup.TextureDictionary;
+                    if (ydrtd != null && ydrtd.Textures != null && ydrtd.Textures.data_items != null)
+                    {
+                        foreach (var tex in ydrtd.Textures.data_items)
+                        {
+                            await SaveTextureAsync(tex, entry, outputpath);
+                        }
+                    }
+                }
+            }
+        }
+
+        private async Task ProcessYftFileAsync(RpfEntry entry, RpfManager rpfman, string outputpath)
+        {
+            UpdateExtractStatus(entry.Path);
+            YftFile yft = rpfman.GetFile<YftFile>(entry);
+            if (yft == null || yft.Fragment == null || yft.Fragment.Drawable == null || yft.Fragment.Drawable.ShaderGroup == null)
+                throw new Exception("Couldn't load fragment or drawable.");
+
+            var ydrtd = yft.Fragment.Drawable.ShaderGroup.TextureDictionary;
+            if (ydrtd != null && ydrtd.Textures != null && ydrtd.Textures.data_items != null)
+            {
+                foreach (var tex in ydrtd.Textures.data_items)
+                {
+                    await SaveTextureAsync(tex, entry, outputpath);
+                }
+            }
+        }
+
+        private async Task SaveTextureAsync(Texture tex, RpfEntry entry, string folder)
+        {
             byte[] dds = DDSIO.GetDDSFile(tex);
 
-            string bpath = folder + "\\" + entry.Name + "_" + tex.Name;
-            string fpath = bpath + ".dds";
-            int c = 1;
-            while (File.Exists(fpath))
+            string textureName = tex.Name + ".dds";
+            string fpath = Path.Combine(folder, textureName);
+
+            if (File.Exists(fpath))
             {
-                fpath = bpath + "_Copy" + c.ToString() + ".dds";
-                c++;
+                return;
             }
 
-            File.WriteAllBytes(fpath, dds);
-
+            using (var fileStream = new FileStream(fpath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                await fileStream.WriteAsync(dds, 0, dds.Length);
+            }
         }
 
 
